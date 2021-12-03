181feb3833a99a2ee9b66dc6335d40db54876961
Alexander-Keyer <alex-keyer@mail.ru>
2021-12-03 19:19:12 +0300

    Revert "Merge pull request #1 from Alexander-Keyer/day_1"
    This reverts commit 0ebc89858c336649c67e87555d2cd82731b0567a, reversing
    changes made to 1d113efd42994e4a92b3816f273c4c787f7e284a.

--- homework_1.1.py
+++
@@ -1,4 +0,0 @@
-a = 2
-b = 131234
-userName = input('Введите своё имя: ')
-print(a, b, 'Ваше имя: ', userName, sep='\n')
--- homework_1.4.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-maxNum = -1
-while n:
-  if n % 10 > maxNum:
-    maxNum = n % 10
-  n //= 10
-print(maxNum)
