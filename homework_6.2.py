e1f84f5ecbafa6c8b6883cbe8204c9a1957982e4
Alexander-Keyer <alex-keyer@mail.ru>
2021-12-13 11:39:54 +0300

    Create homework_6.2.py

---
+++ homework_6.2.py
@@ -0,0 +1,11 @@
+class Road:
+  length = 5000
+  width = 20
+  def __init__(self, mass, thickness):
+    self.mass = self.mass
+    
+    
+  def calculate_road_mass(self, mass, thickness):
+    return self.length * self.width * mass * thickness
+
+print(f'{Road.calculate_road_mass(Road, 25, 5) // 1000} т')
